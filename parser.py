31602c7a49dbfecfbe4ecb051fd367c85d406708
Andreas Nauleau <andreas@adap.tv>
2013-07-14 19:02:23 -0700

    begins modification of py parser

--- parser.py
+++ parser.py
@@ -2,18 +2,30 @@ import sys #imports sys module so file can be passed as arg from command line
 import xml.etree.cElementTree as etree #imports etree to parse XML
 
 # filename = sys.argv[1] #assigns second argument to filename - first is solution.py
-tree = etree.parse('routes.xml') #sets the passed xml file as the tree element
+# schema = etree.parse('schema.xml') #sets the passed xml file as the tree element
+db = etree.parse('db.xml') #sets the passed xml file as the tree element
 root = tree.getroot() #assigns the root of the xml file to root
 
-places = {}
-lat = 'lat'
-lon = 'lon'
+pathfinder = {
+    "admin": {},
+    "canoes": {},
+    "codes": {},
+    "paths": {},
+    "peopleRoles": {},
+    "places": {},
+    "trips": {},
+    "tripCanoes": {},
+    "tripPeople": {},
+    "tripRoutes": {},
+    "xLog": {},
+    "xVLog": {}
+}
 
-for place in root.findall('table'):
-  places[place[0].text] = {}
-  places[place[0].text]['name'] = place[1].text
-  places[place[0].text]['lat']  = place[2].text
-  places[place[0].text]['lon']  = place[3].text
+# new obj for each table
+# for each column in row insert in to obj
 
-print(places)
+
+# for table in schema.findall('table'):
+#     pathfincer[table.name] = {}
+print(pathfinder)
 
